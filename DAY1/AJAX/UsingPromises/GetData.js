287f6ef581d80eea03e8a74f11b4a5ac9f543c8e
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2025-07-01 16:18:15 +0530

    ✨ using Fetch API

--- DAY1/AJAX/UsingPromises/GetData.js
+++ DAY1/AJAX/UsingPromises/GetData.js
@@ -1,15 +1,3 @@
 function GetData() {
-  return new Promise(function (resolve, reject) {
-    // AJAX Logic for fetching data
-    var xmlHttpReq = new XMLHttpRequest();
-    xmlHttpReq.open("GET", "https://jsonplaceholder.typicode.com/posts");
-    xmlHttpReq.onreadystatechange = function () {
-      if (xmlHttpReq.status === 200 && xmlHttpReq.readyState === 4) {
-        resolve(xmlHttpReq.responseText);
-      } else if (xmlHttpReq.status !== 200 && xmlHttpReq.readyState === 4) {
-        reject("Error : " + xmlHttpReq.status);
-      }
-    };
-    xmlHttpReq.send(); // actually places the async call
-  });
+  return axios.get("https://jsonplaceholder.typicode.com/postsss");
 }
