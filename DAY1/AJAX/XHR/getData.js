9b3c3c0fc7067a80fe591cc80576cf54ba68aedf
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2025-07-01 15:07:35 +0530

    ✨ Using Promises

--- DAY1/AJAX/XHR/getData.js
+++ DAY1/AJAX/XHR/getData.js
@@ -1,10 +1,12 @@
-function GetData() {
+function GetData(callback) {
   // AJAX Logic for fetching data
   var xmlHttpReq = new XMLHttpRequest();
   xmlHttpReq.open("GET", "https://jsonplaceholder.typicode.com/posts");
   xmlHttpReq.onreadystatechange = function () {
     if (xmlHttpReq.status === 200 && xmlHttpReq.readyState === 4) {
-      console.log(xmlHttpReq.responseText);
+      callback(xmlHttpReq.responseText, null);
+    } else if (xmlHttpReq.status !== 200 && xmlHttpReq.readyState === 4) {
+      callback(null, "Error !" + xmlHttpReq.status);
     }
   };
   xmlHttpReq.send(); // actually places the async call
