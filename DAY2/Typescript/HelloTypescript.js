10e36156ad6aef795ea54b6bf8e371f53324ec66
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2025-07-02 12:56:40 +0530

    ✨ Inhertaince , Rest, Optional , Default Parameters

--- DAY2/Typescript/HelloTypescript.js
+++ DAY2/Typescript/HelloTypescript.js
@@ -1,5 +1,20 @@
 //var msg = "Hello Typescript !"; // Type Inferencing
 // msg = 100; // Error
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var msg = "Hello Typescript !"; // Type annotation
 console.log(msg);
 var age = 18;
@@ -10,3 +25,65 @@ x = 10;
 x = "Hi";
 x = [10, 20];
 x = false;
+function Add(x, y) {
+    if (x == 0) {
+        return "X must be greater than 0 !";
+    }
+    return x + y;
+}
+// Add("Hello", "Typescript"); // Error !
+var result = Add(10, 20);
+// Optional parameters
+// function PrintBook(author?: string, title?: string, numOfPages?: number) {
+//   console.log(author, title, numOfPages);
+// }
+// PrintBook();
+// PrintBook("XYZ");
+// PrintBook("Dr. APJ Abdul Kalam", "Wings Of Fire");
+// Default parameters
+// function PrintBook(author: string, title: string, numOfPages: number = 100) {
+//   console.log(author, title, numOfPages);
+// }
+// PrintBook("Dr. APJ Abdul Kalam", "Wings Of Fire", 400);
+// var cars = ["BMW", "AUDI", "Merc"];
+// var cars: string[] = ["BMW", "AUDI", "Merc"];
+// Rest Parameters
+function PrintBook(author) {
+    var titles = [];
+    for (var _i = 1; _i < arguments.length; _i++) {
+        titles[_i - 1] = arguments[_i];
+    }
+    console.log(author, titles);
+}
+PrintBook("Dr. APJ Abdul Kalam", "Wings Of Fire", "India 2020");
+// class
+var Car = /** @class */ (function () {
+    function Car(name, speed) {
+        if (name === void 0) { name = "BMW"; }
+        if (speed === void 0) { speed = 200; }
+        this.id = 1;
+        this.name = name;
+        this.speed = speed;
+    }
+    Car.prototype.accelerate = function () {
+        return "The car " + this.name + " is running at " + this.speed + " kmph !";
+    };
+    return Car;
+}());
+// var carObj = new Car();
+// console.log(carObj.accelerate());
+var JamesBondCar = /** @class */ (function (_super) {
+    __extends(JamesBondCar, _super);
+    function JamesBondCar(name, speed, canFly, canSubmerge) {
+        var _this = _super.call(this, name, speed) || this;
+        _this.canFly = canFly;
+        _this.canSubmerge = canSubmerge;
+        return _this;
+    }
+    JamesBondCar.prototype.accelerate = function () {
+        return _super.prototype.accelerate.call(this) + " Can it fly ? " + this.canFly;
+    };
+    return JamesBondCar;
+}(Car));
+var jbc = new JamesBondCar("Aston Martin", 300, true, true);
+console.log(jbc.accelerate());
--- DAY2/Typescript/HelloTypescript.ts
+++ DAY2/Typescript/HelloTypescript.ts
@@ -22,3 +22,68 @@ function Add(x: number, y: number): number | string {
 
 // Add("Hello", "Typescript"); // Error !
 var result: number | string = Add(10, 20);
+
+// Optional parameters
+// function PrintBook(author?: string, title?: string, numOfPages?: number) {
+//   console.log(author, title, numOfPages);
+// }
+
+// PrintBook();
+// PrintBook("XYZ");
+// PrintBook("Dr. APJ Abdul Kalam", "Wings Of Fire");
+
+// Default parameters
+// function PrintBook(author: string, title: string, numOfPages: number = 100) {
+//   console.log(author, title, numOfPages);
+// }
+// PrintBook("Dr. APJ Abdul Kalam", "Wings Of Fire", 400);
+
+// var cars = ["BMW", "AUDI", "Merc"];
+// var cars: string[] = ["BMW", "AUDI", "Merc"];
+
+// Rest Parameters
+function PrintBook(author: string, ...titles: string[]): void {
+  console.log(author, titles);
+}
+
+PrintBook("Dr. APJ Abdul Kalam", "Wings Of Fire", "India 2020");
+
+// class
+
+class Car {
+  private id: number = 1;
+  name: string;
+  speed: number;
+
+  constructor(name = "BMW", speed = 200) {
+    this.name = name;
+    this.speed = speed;
+  }
+  accelerate(): string {
+    return "The car " + this.name + " is running at " + this.speed + " kmph !";
+  }
+}
+// var carObj = new Car();
+// console.log(carObj.accelerate());
+
+class JamesBondCar extends Car {
+  canFly: boolean;
+  canSubmerge: boolean;
+  constructor(
+    name: string,
+    speed: number,
+    canFly: boolean,
+    canSubmerge: boolean,
+  ) {
+    super(name, speed);
+    this.canFly = canFly;
+    this.canSubmerge = canSubmerge;
+  }
+
+  accelerate(): string {
+    return super.accelerate() + " Can it fly ? " + this.canFly;
+  }
+}
+
+var jbc = new JamesBondCar("Aston Martin", 300, true, true);
+console.log(jbc.accelerate());
