f7c96e2c8c027662ecc2143d8792982104c5dc0a
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2025-07-03 10:50:26 +0530

    ✨ Displaying images

--- DAY2/my-react-app/src/components/course/course.component.jsx
+++ DAY2/my-react-app/src/components/course/course.component.jsx
@@ -4,6 +4,7 @@ export default class Course extends Component {
   render() {
     return (
       <div>
+        <img src={this.props.coursedetails.imageUrl} alt="" width="200px" />
         <h1>{this.props.coursedetails.title}</h1>
         <p>{this.props.coursedetails.price}</p>
         <p>{this.props.coursedetails.rating}</p>
