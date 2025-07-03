bd6fb1be7d405c926b886fe339c1541cad19f9c0
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2025-07-03 14:46:11 +0530

    ✨ using useState() in functional component

--- DAY2/my-react-app/src/components/app/App.jsx
+++ DAY2/my-react-app/src/components/app/App.jsx
@@ -1,18 +1,27 @@
 import React from "react";
 import ListOfCourses from "../listofcourses/listofcourses";
 import FMessage from "../functional/message.functional";
+import Counter from "../functional/counter";
 
-class App extends React.Component {
-  render() {
-    // return <ListOfCourses />;
-    return (
-      <div>
-        <FMessage msg="Hello" />
-        <FMessage msg="Hey" />
-        <FMessage msg="Hola" />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div>
+      <Counter />
+    </div>
+  );
 }
+
+// class App extends React.Component {
+//   render() {
+//     // return <ListOfCourses />;
+//     return (
+//       <div>
+//         <FMessage msg="Hello" />
+//         <FMessage msg="Hey" />
+//         <FMessage msg="Hola" />
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
--- DAY2/my-react-app/src/components/functional/message.functional.jsx
+++ DAY2/my-react-app/src/components/functional/message.functional.jsx
@@ -6,7 +6,6 @@
 //   return <h1> Hey !</h1>;
 // };
 
-var FMessage = props => 
-<h1> {props.msg}</h1>;
+var FMessage = props => <h1> {props.msg}</h1>;
 
 export default FMessage;
