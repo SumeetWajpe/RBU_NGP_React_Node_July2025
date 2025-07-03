745263cb86b0c75e92b90adbe09102792e9716ae
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2025-07-03 12:23:28 +0530

    ✨ Ratings

--- DAY2/my-react-app/src/components/course/course.component.jsx
+++ DAY2/my-react-app/src/components/course/course.component.jsx
@@ -1,7 +1,17 @@
 import React, { Component } from "react";
 
 export default class Course extends Component {
+  incrementLikes() {
+    console.log("U clicked !");
+  }
+
   render() {
+    let ratings = [];
+    for (let index = 0; index < this.props.coursedetails.rating; index++) {
+      ratings.push(
+        <i className="fa-solid fa-star" style={{ color: "orange" }}></i>,
+      );
+    }
     return (
       <div className="col-md-3 my-1">
         <div className="card">
@@ -13,12 +23,17 @@ export default class Course extends Component {
           />
           <div className="card-body">
             <h5 className="card-title">{this.props.coursedetails.title}</h5>
-            <p className="card-text">₹.{this.props.coursedetails.price}</p>
             <p className="card-text">
-              Rating : {this.props.coursedetails.rating}
-            </p>
+              {/* Rating : {this.props.coursedetails.rating} */}
 
-            <button class="btn btn-primary">
+              {ratings}
+            </p>
+            <p className="card-text">₹.{this.props.coursedetails.price}</p>
+
+            <button
+              class="btn btn-primary"
+              onClick={() => this.incrementLikes()}
+            >
               {this.props.coursedetails.likes}{" "}
               <i className="fa-solid fa-thumbs-up"></i>
             </button>
