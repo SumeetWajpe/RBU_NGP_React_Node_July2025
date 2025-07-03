cd41da216932d51fefe61a608584530f07c0d990
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2025-07-03 16:02:26 +0530

    ✨ Using Router

--- DAY2/my-react-app/src/components/app/App.jsx
+++ DAY2/my-react-app/src/components/app/App.jsx
@@ -3,13 +3,22 @@ import ListOfCourses from "../listofcourses/listofcourses";
 import FMessage from "../functional/message.functional";
 import Counter from "../functional/counter";
 import Posts from "../posts/posts";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
-    <div>
-      {/* <Counter /> */}
-      <Posts />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" Component={ListOfCourses} />
+        <Route path="/posts" Component={Posts} />
+        <Route path="/counter" Component={Counter} />
+        <Route
+          path="*"
+          element={
+            <img src="https://miro.medium.com/v2/resize:fit:1400/0*QOZm9X5er1Y0r5-t" />
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
