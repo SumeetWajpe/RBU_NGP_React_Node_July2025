994e616bbb50cf50a616993a3b37cfbc96e50992
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2025-07-05 10:44:11 +0530

    ✨ Using Express Generator

--- DAY4/NodeJS/ExpressBasics/src/server/app.js
+++ DAY4/NodeJS/ExpressBasics/src/server/app.js
@@ -4,28 +4,30 @@ const path = require("path");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  //   res.send("Hello World!");
-  res.sendFile("Index.html", { root: __dirname + "/../client" });
-});
-app.get("/styles.css", (req, res) => {
-  //   res.send("Hello World!");
-  res.sendFile("styles.css", { root: __dirname + "/../client" });
-});
-app.get("/client.js", (req, res) => {
-  //   res.send("Hello World!");
-  res.sendFile("client.js", { root: __dirname + "/../client" });
-});
-app.get("/products", (req, res) => {
-  var products = [
-    { id: 1, name: "LED TV", price: 50000 },
-    { id: 2, name: "MacBookPro", price: 250000 },
-    { id: 3, name: "DJI Mavic Pro", price: 450000 },
-  ];
+// products.js
+var products = [
+  { id: 1, name: "LED TV", price: 50000 },
+  { id: 2, name: "MacBookPro", price: 250000 },
+  { id: 3, name: "DJI Mavic Pro", price: 450000 },
+];
 
+// middleware
+app.use(express.static("src/client")); // middleware -> extra funcyionality on top of default
+app.use(express.json());
+
+// api / endpoints
+app.get("/products", (req, res) => {
   res.json(products);
 });
 
+app.post("/newproduct", (req, res) => {
+  const payload = req.body;
+  console.log(payload);
+  products.push(payload);
+  res.send("Product added successfully !");
+});
+
+// server
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
